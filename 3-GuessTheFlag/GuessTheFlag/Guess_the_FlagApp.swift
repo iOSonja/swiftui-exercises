9608be607528ab1fb002bfa2f9c064bab356b042
Sonja Ek <sonja3k@icloud.com>
2020-11-10 15:51:50 +0200

    Country Flag quiz app. Partially tutorial-based code, partially written by me.

---
+++ 3-GuessTheFlag/GuessTheFlag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Sonja Ek on 23.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
