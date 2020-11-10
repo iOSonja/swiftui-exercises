2a7d33ae7295c639d66b1f90ab21b2592db17369
Sonja Ek <sonja3k@icloud.com>
2020-11-10 12:33:13 +0200

    Initial Commit

---
+++ swiftui-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-challenge
+//
+//  Created by Sonja Ek on 10.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-challenge/swiftui_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_challengeApp.swift
+//  swiftui-challenge
+//
+//  Created by Sonja Ek on 10.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
