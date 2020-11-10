a5f5f9c4be840cb88658499883dd68a5a6dd89c7
Sonja Ek <sonja3k@icloud.com>
2020-11-10 15:08:06 +0200

    First version of a bill splitter app. Some of the functionalities are made by me but the base is from a tutorial.

---
+++ 1-WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Sonja Ek on 18.10.2020.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var checkNumberOfPeople = ""
+    @State private var tipPercentage = 2
+    @State private var useRedText = false
+        
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    
+    var totalAmount: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double((Double(checkNumberOfPeople) ?? 0) + 2)
+        let grandTotal = totalAmount
+
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                
+                    TextField("Number of people", text: $checkNumberOfPeople)
+                        .keyboardType(.decimalPad)
+                    
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Total amount")) {
+                    Text("\(totalAmount, specifier: "%.2f")€")
+                        .foregroundColor(useRedText ? .red : .black)
+                }
+                
+                Section(header: Text("Amount per person")) {
+                    Text("\(totalPerPerson, specifier: "%.2f")€")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 1-WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sonja Ek on 18.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
