ba765438ac2130b0b76bf69e70e1e95f093c90ff
Sonja Ek <sonja3k@icloud.com>
2020-11-19 13:15:27 +0200

    File structure changed

---
+++ Tutorial-based Exercises/2-UnitConvo/2-UnitConvo/__UnitConvoApp.swift
@@ -0,0 +1,17 @@
+//
+//  __UnitConvoApp.swift
+//  2-UnitConvo
+//
+//  Created by Sonja Ek on 12.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct __UnitConvoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
