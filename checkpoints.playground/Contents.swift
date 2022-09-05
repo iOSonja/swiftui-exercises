71ef5dce7e645d1ffc0d3219142ec35fa0a3d4da
Sonja Ek <sonja3k@icloud.com>
2022-09-05 13:22:43 +0300

    Upload checkpoints.playground

---
+++ checkpoints.playground/Contents.swift
@@ -0,0 +1,62 @@
+import Cocoa
+
+// Checkpoint 1
+let tempInCelsius = -40.0
+let tempInFahrenheit = 9 * tempInCelsius / 5 + 32
+print("Temperature in Celsius is \(tempInCelsius)")
+print("Temperature in Fahrenhet is \(tempInFahrenheit)")
+print()
+
+// Checkpoint 2
+var array = ["Elsa", "Hans"]
+array.append("Anna")
+array.append("Elsa")
+array.append("Kristof")
+array.append("Olaf")
+print(array.count)
+let unique = Set(array)
+print(unique.count)
+print()
+
+// Checkpoint 3
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if i.isMultiple(of: 3) {
+        print("Fizz")
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
+print()
+
+// Checkpoint 4
+enum RootError: Error {
+    case outOfBounds, noRoot
+}
+
+func findSquareRoot(of number: Int) throws -> Int {
+    if number < 1 || number > 10_000 {
+        throw RootError.outOfBounds
+    }
+    for i in 1...100 {
+        if i * i == number {
+            return i
+        }
+    }
+    throw RootError.noRoot
+}
+
+for _ in 1...10 {
+    let x = Int.random(in: -10...110)
+    do {
+        try print("The square root of \(x) is ", findSquareRoot(of: x))
+    } catch RootError.outOfBounds{
+        print("Error: number \(x) is out of bounds")
+    } catch RootError.noRoot {
+        print("Error: \(x) has no root")
+    }
+}
+
