b41333bc1113277ff6aa41cfc2dc9e43e2ffd376
Sonja Ek <sonja3k@icloud.com>
2022-09-07 09:29:51 +0300

    Add checkpoint 6

--- checkpoints.playground/Contents.swift
+++ checkpoints.playground/Contents.swift
@@ -70,3 +70,31 @@ for number in formattedNumbers {
     print(number)
 }
 print()
+
+// Checkpoint 6
+struct Car {
+    public let model: String
+    public let nbrOfSeats: Int
+    private(set) var currentGear: Int
+
+    mutating func changeGears(by change: Int) {
+        if currentGear + change > 10 {
+            print("I don't have that many gears")
+        } else if currentGear + change < 1 {
+            print("Gear number can't be less than one")
+        } else {
+            currentGear += change
+        }
+    }
+}
+
+var fiesta = Car(model: "Ford Fiesta", nbrOfSeats: 5, currentGear: 6)
+print("Current gear: \(fiesta.currentGear)")
+fiesta.changeGears(by: 3)
+print("Current gear: \(fiesta.currentGear)")
+fiesta.changeGears(by: -12)
+print("Current gear: \(fiesta.currentGear)")
+fiesta.changeGears(by: 2)
+print("Current gear: \(fiesta.currentGear)")
+
+print()
