2eb5b1fae04d483906d70f63ca83cc3a528bc652
Sonja Ek <sonja3k@icloud.com>
2022-09-14 21:35:26 +0300

    Upload MVP WeSplit (iOS 15.0)

---
+++ 01_WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sonja Ek on 14.9.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
