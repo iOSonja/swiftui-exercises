30d7ccc0b3ed4e9d9a261b375cf94c3248e8c09e
Sonja Ek <sonja3k@icloud.com>
2022-09-17 21:53:35 +0300

    Upload MVP GuessTheFlag (iOS 15.0)

---
+++ 03_GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sonja Ek on 17.9.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
