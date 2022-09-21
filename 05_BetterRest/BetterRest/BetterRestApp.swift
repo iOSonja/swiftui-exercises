d184e5753f63a412214815aac5a1cda2cf6af825
Sonja Ek <sonja3k@icloud.com>
2022-09-21 07:24:16 +0300

    Upload MVP BetterRest (iOS 15.0)

---
+++ 05_BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sonja Ek on 21.9.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
