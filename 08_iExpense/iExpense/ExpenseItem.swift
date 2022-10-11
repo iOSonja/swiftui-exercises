e26ebde8bbedc5fdfb46e88ab63aa9d6eacac840
Sonja Ek <sonja3k@icloud.com>
2022-10-11 21:39:27 +0300

    Upload MVP iExpense (iOS 15.0)

---
+++ 08_iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,15 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Sonja Ek on 11.10.2022.
+//
+
+import Foundation
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
---
+++ 08_iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Sonja Ek on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
