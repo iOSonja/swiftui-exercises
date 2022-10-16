b88f0f44f808118752255116d8899beaadf0a8a3
Sonja Ek <sonja3k@icloud.com>
2022-10-16 12:15:40 +0300

    Refactor crew scroll view into a separate SwiftUI view

---
+++ 09_Moonshot/Moonshot/CrewMember.swift
@@ -0,0 +1,13 @@
+//
+//  CrewMember.swift
+//  Moonshot
+//
+//  Created by Sonja Ek on 16.10.2022.
+//
+
+import Foundation
+
+struct CrewMember {
+    let role: String
+    let astronaut: Astronaut
+}
--- 09_Moonshot/Moonshot/MissionView.swift
+++ 09_Moonshot/Moonshot/MissionView.swift
@@ -7,12 +7,43 @@
 
 import SwiftUI
 
-struct MissionView: View {
-    struct CrewMember {
-        let role: String
-        let astronaut: Astronaut
-    }
+struct CrewView: View {
+    let crew: [CrewMember]
 
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(crew, id: \.role) { crewMember in
+                    NavigationLink {
+                        AstronautView(astronaut: crewMember.astronaut)
+                    } label: {
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 104, height: 72)
+                                .clipShape(Capsule())
+                                .overlay(
+                                    Capsule()
+                                        .strokeBorder(.white, lineWidth: 1)
+                                )
+
+                            VStack(alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .foregroundColor(.white)
+                                    .font(.headline)
+                                Text(crewMember.role)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct MissionView: View {
     let mission: Mission
     let crew: [CrewMember]
 
@@ -53,35 +84,7 @@ struct MissionView: View {
                     }
                     .padding(.horizontal)
 
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack {
-                            ForEach(crew, id: \.role) { crewMember in
-                                NavigationLink {
-                                    AstronautView(astronaut: crewMember.astronaut)
-                                } label: {
-                                    HStack {
-                                        Image(crewMember.astronaut.id)
-                                            .resizable()
-                                            .frame(width: 104, height: 72)
-                                            .clipShape(Capsule())
-                                            .overlay(
-                                                Capsule()
-                                                    .strokeBorder(.white, lineWidth: 1)
-                                            )
-
-                                        VStack(alignment: .leading) {
-                                            Text(crewMember.astronaut.name)
-                                                .foregroundColor(.white)
-                                                .font(.headline)
-                                            Text(crewMember.role)
-                                                .foregroundColor(.secondary)
-                                        }
-                                    }
-                                    .padding(.horizontal)
-                                }
-                            }
-                        }
-                    }
+                    CrewView(crew: crew)
                 }
                 .padding(.bottom)
             }
