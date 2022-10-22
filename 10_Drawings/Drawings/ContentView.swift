089030f88cf88200b9edadd1626d26b1fc61c746
Sonja Ek <sonja3k@icloud.com>
2022-10-22 13:33:25 +0300

    Remove paddings so that all listings fit on screen

--- 10_Drawings/Drawings/ContentView.swift
+++ 10_Drawings/Drawings/ContentView.swift
@@ -15,61 +15,51 @@ struct ContentView: View {
                     Snowman()
                 } label: {
                     Text("Snowman")
-                        .padding()
                 }
                 NavigationLink {
                     Flower()
                 } label: {
                     Text("Flower")
-                        .padding()
                 }
                 NavigationLink {
                     ImageFrame()
                 } label: {
                     Text("Image Frame")
-                        .padding()
                 }
                 NavigationLink {
                     ColorCyclingCircle()
                 } label: {
                     Text("Color Cycling Circle")
-                        .padding()
                 }
                 NavigationLink {
                     Blur()
                 } label: {
                     Text("Blur")
-                        .padding()
                 }
                 NavigationLink {
                     ColorFilter()
                 } label: {
                     Text("Color Filter")
-                        .padding()
                 }
                 NavigationLink {
                     OverlappingCircles()
                 } label: {
                     Text("Overlapping Circles")
-                        .padding()
                 }
                 NavigationLink {
                     AnimatedTrapezoid()
                 } label: {
                     Text("Animated Trapezoid")
-                        .padding()
                 }
                 NavigationLink {
                     AnimatedCheckerboard()
                 } label: {
                     Text("Animated Checkerboard")
-                        .padding()
                 }
                 NavigationLink {
                     Arrow()
                 } label: {
                     Text("Arrow")
-                        .padding()
                 }
             }
             .navigationTitle("Drawings")
