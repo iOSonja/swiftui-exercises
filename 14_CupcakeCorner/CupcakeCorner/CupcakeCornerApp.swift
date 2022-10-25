87797845c99e7058c5726893fb17e201995a8583
Sonja Ek <sonja3k@icloud.com>
2022-10-25 19:38:14 +0300

    Add UI for CupcakeCorner

---
+++ 14_CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Sonja Ek on 25.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
