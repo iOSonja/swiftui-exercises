8985034c702c58aa39e5094d36adb9d1c30a5928
Sonja Ek <sonja3k@icloud.com>
2022-11-01 04:49:14 +0200

    Create Movie entity & NSManagedObject subclass for it

---
+++ 16_CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Sonja Ek on 1.11.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
