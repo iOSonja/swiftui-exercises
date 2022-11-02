0b764ec13a17882e0b0d612cfef24749cdec35e6
Sonja Ek <sonja3k@icloud.com>
2022-11-02 20:17:19 +0200

    Play with one-to-many relatioships in Core Data

---
+++ 16_CoreDataProject/CoreDataProject/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Sonja Ek on 2.11.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ 16_CoreDataProject/CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Sonja Ek on 2.11.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
