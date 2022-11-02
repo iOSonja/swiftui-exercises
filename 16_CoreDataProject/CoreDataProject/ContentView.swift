154288d8f88d870f7e89352e48b238c34d71c7e2
Sonja Ek <sonja3k@icloud.com>
2022-11-02 20:26:34 +0200

    Show example views in a list in ContentView

--- 16_CoreDataProject/CoreDataProject/ContentView.swift
+++ 16_CoreDataProject/CoreDataProject/ContentView.swift
@@ -5,37 +5,32 @@
 //  Created by Sonja Ek on 1.11.2022.
 //
 
-import CoreData
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe IN %@", ["Aliens", "Firefly", "Star Trek"])) var ships: FetchedResults<Ship>
-
     var body: some View {
-        VStack {
-            List(ships, id: \.self) { ship in
-                Text(ship.name ?? "Unknown name")
-            }
+        NavigationView {
+            List {
+                NavigationLink {
+                    ShipsView()
+                } label: {
+                    Text("Filtering @FetchRequest using NSPredicate")
+                }
+                .padding()
 
-            Button("Add Examples") {
-                let ship1 = Ship(context: moc)
-                ship1.name = "Enterprise"
-                ship1.universe = "Star Trek"
-
-                let ship2 = Ship(context: moc)
-                ship2.name = "Defiant"
-                ship2.universe = "Star Trek"
-
-                let ship3 = Ship(context: moc)
-                ship3.name = "Millennium Falcon"
-                ship3.universe = "Star Wars"
-
-                let ship4 = Ship(context: moc)
-                ship4.name = "Executor"
-                ship4.universe = "Star Wars"
-
-                try? moc.save()
+                NavigationLink {
+                    DynamicFilteringView()
+                } label: {
+                    Text("Dynamically filtering @FetchRequest with SwiftUI")
+                }
+                .padding()
+                
+                NavigationLink {
+                    CandyView()
+                } label: {
+                    Text("One-to-many relationships with Core Data, SwiftUI, and @FetchRequest")
+                }
+                .padding()
             }
         }
     }
