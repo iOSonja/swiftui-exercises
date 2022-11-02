6c2d9f283fc7f97e831b6c89abe016aa2c4824ef
Sonja Ek <sonja3k@icloud.com>
2022-11-02 19:41:27 +0200

    Change FilteredList to be a multipurpose tool

--- 16_CoreDataProject/CoreDataProject/FilteredList.swift
+++ 16_CoreDataProject/CoreDataProject/FilteredList.swift
@@ -5,20 +5,25 @@
 //  Created by Sonja Ek on 1.11.2022.
 //
 
+import CoreData
 import SwiftUI
 
-struct FilteredList: View {
-    @FetchRequest var fetchRequest: FetchedResults<Singer>
+struct FilteredList<T: NSManagedObject, Content: View>: View {
+    @FetchRequest var fetchRequest: FetchedResults<T>
+    
+    // this is our content closure; we'll call this once for each item in the list
+    let content: (T) -> Content
 
     var body: some View {
-        List(fetchRequest, id: \.self) { singer in
-            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        List(fetchRequest, id: \.self) { item in
+            self.content(item)
         }
     }
 
-    init(filter: String) {
+    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
         // The underscore is there because we’re not writing to the fetched results object
         // inside our fetch request, but instead writing a wholly new fetch request
-        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+        self.content = content
     }
 }
