6d5bb8bc63d5e1cdce60d5d96b6226eb5ed1044f
Sonja Ek <sonja3k@icloud.com>
2022-11-07 20:11:11 +0200

    Create helper files for Instafilter

---
+++ 18_Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Sonja Ek on 7.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
