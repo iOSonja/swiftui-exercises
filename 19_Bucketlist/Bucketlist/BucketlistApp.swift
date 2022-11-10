e8b64510f5e22c2e55bdd8c6babe6f467c233fb3
Sonja Ek <sonja3k@icloud.com>
2022-11-10 20:35:11 +0200

    Create the Bucketlist project with Face ID permission check

---
+++ 19_Bucketlist/Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Sonja Ek on 10.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
