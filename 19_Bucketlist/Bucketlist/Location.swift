441991afb191f095b056fd07c89a268734c9064e
Sonja Ek <sonja3k@icloud.com>
2022-11-11 09:12:51 +0200

    Create example location

--- 19_Bucketlist/Bucketlist/Location.swift
+++ 19_Bucketlist/Bucketlist/Location.swift
@@ -18,4 +18,6 @@ struct Location: Identifiable, Codable, Equatable {
     var coordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
+    
+    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Where Queen Elizabeth lived with her dorgis 😢", latitude: 51.501, longitude: -0.141)
 }
