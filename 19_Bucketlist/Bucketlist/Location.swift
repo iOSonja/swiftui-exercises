5604489e9ddbb542f5141a0d7a80d99e9770b053
Sonja Ek <sonja3k@icloud.com>
2022-11-11 09:14:28 +0200

    Add Equatable conformance to Location

--- 19_Bucketlist/Bucketlist/Location.swift
+++ 19_Bucketlist/Bucketlist/Location.swift
@@ -18,6 +18,10 @@ struct Location: Identifiable, Codable, Equatable {
     var coordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
-    
+
     static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Where Queen Elizabeth lived with her dorgis 😢", latitude: 51.501, longitude: -0.141)
+
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
