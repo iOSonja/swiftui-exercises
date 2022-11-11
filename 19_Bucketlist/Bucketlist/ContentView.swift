a4ae2d3ba0e5945aab9492feddbda9961661c16d
Sonja Ek <sonja3k@icloud.com>
2022-11-11 09:42:35 +0200

    Make sure location names aren't clipped on the map

--- 19_Bucketlist/Bucketlist/ContentView.swift
+++ 19_Bucketlist/Bucketlist/ContentView.swift
@@ -27,6 +27,7 @@ struct ContentView: View {
                             .clipShape(Circle())
 
                         Text(location.name)
+                            .fixedSize()
                     }
                     .onTapGesture {
                         selectedPlace = location
