820e5f733f3f953433c5e5ef0ca2d09ca3febed0
Sonja Ek <sonja3k@icloud.com>
2022-11-20 19:28:57 +0200

    Allow uploading images from device gallery

---
+++ 21_Acquaintances/Acquaintances/AcquaintancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcquaintancesApp.swift
+//  Acquaintances
+//
+//  Created by Sonja Ek on 20.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AcquaintancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
