fd3d5a189c631379bf9c07a75e7527d444e57768
Sonja Ek <sonja3k@icloud.com>
2022-11-25 19:45:54 +0200

    Save images with names & show the added people on a list

--- 21_Acquaintances/Acquaintances/AddView.swift
+++ 21_Acquaintances/Acquaintances/AddView.swift
@@ -7,30 +7,77 @@
 
 import SwiftUI
 
+struct SuccessView: View {
+    let inputImage: UIImage
+
+    var body: some View {
+        Image(uiImage: inputImage)
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
 struct AddView: View {
-    let image: Image?
+    @Environment(\.dismiss) var dismiss
+
     @State private var name = ""
+
+    @Binding var isDone: Bool
+    @Binding var people : [Person]
+
+    let inputImage: UIImage?
+    let url: URL
+
+    enum ImageState {
+        case success, failed
+    }
 
     var body: some View {
         NavigationView {
             List {
-                image?
-                    .resizable()
-                    .scaledToFit()
+                if let rawImage = inputImage {
+                    SuccessView(inputImage: rawImage)
+                } else {
+                    FailedView()
+                }
 
                 TextField("Person's name", text: $name)
             }
             .toolbar {
                 ToolbarItem(placement: .primaryAction) {
-                    Button("Save", action: save)
-                        .disabled(image == nil)
+                    Button("Save") {
+                        save()
+                        isDone = true
+                        dismiss()
+                    }
+                        .disabled(inputImage == nil)
                 }
             }
         }
     }
 
     func save() {
-        // to be continued
+        do {
+            let person = Person(id: UUID(), name: name)
+            people.append(person)
+            let encoder = JSONEncoder()
+            let json = try encoder.encode(people)
+
+            try? json.write(to: url, options: [.atomic, .completeFileProtection])
+
+            if let jpegData = inputImage?.jpegData(compressionQuality: 0.8) {
+                try? jpegData.write(to: FileManager.default.getDocumentsDirectory.appendingPathComponent(person.id.uuidString  + ".jpg"), options: [.atomic, .completeFileProtection])
+            }
+            print("great success with saving")
+        } catch {
+            print("Couldn't save person")
+        }
     }
 }
 
--- 21_Acquaintances/Acquaintances/ContentView.swift
+++ 21_Acquaintances/Acquaintances/ContentView.swift
@@ -8,41 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
     @State private var inputImage: UIImage?
+    @State private var people = [Person]()
+    @State private var savePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("people.json")
+
     @State private var showingImagePicker = false
     @State private var showingAddView = false
+    @State private var newPersonAdded = false
 
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-
-            Button("Select Image") {
-               showingImagePicker = true
+        NavigationView {
+            List(people, id: \.id) { person in
+                HStack {
+                    Text(person.name)
+                    Spacer()
+                    person.image?
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .scaledToFill()
+                        .padding()
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Add person") {
+                        showingImagePicker = true
+                    }
+                }
+            }
+            .task {
+                loadData()
+            }
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
+            .onChange(of: inputImage) { _ in
+                showingAddView = true
+            }
+            .onChange(of: newPersonAdded) { _ in
+                loadData()
+            }
+            .sheet(isPresented: $showingAddView) {
+                AddView(isDone: $newPersonAdded, people: $people, inputImage: inputImage, url: savePath)
             }
         }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker(image: $inputImage)
-        }
-        .sheet(isPresented: $showingAddView) {
-            AddView(image: image)
-        }
-        .onChange(of: inputImage) { _ in
-            loadImage()
-            showingAddView = true
+    }
+
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+
+    func loadData() {
+        do {
+            let data = try Data(contentsOf: savePath)
+            let decoder = JSONDecoder()
+            let decodedPeople = try decoder.decode([Person].self, from: data)
+            people = decodedPeople.sorted()
+            print("success with loadData")
+        } catch {
+            people = []
+            print("Unable to load people")
         }
     }
 
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+    func loadImage(inputImage: UIImage?) -> Image? {
+        guard let inputImage = inputImage else { return nil }
+        return Image(uiImage: inputImage)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
