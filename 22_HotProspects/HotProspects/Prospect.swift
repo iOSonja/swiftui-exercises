88d3d67e3f692edde105b6ccb540b3626afbd117
Sonja Ek <sonja3k@icloud.com>
2022-12-01 21:51:59 +0200

    Make data handling safer with encapsulation

--- 22_HotProspects/HotProspects/Prospect.swift
+++ 22_HotProspects/HotProspects/Prospect.swift
@@ -15,10 +15,11 @@ class Prospect: Identifiable, Codable {
 }
 
 @MainActor class Prospects: ObservableObject {
-    @Published var people: [Prospect]
+    @Published private(set) var people: [Prospect]
+    let saveKey = "SavedData"
 
     init() {
-        if let data = UserDefaults.standard.data(forKey: "SavedData") {
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
             if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
                 people = decoded
                 return
@@ -28,10 +29,15 @@ class Prospect: Identifiable, Codable {
         people = []
     }
 
-    func save() {
+    private func save() {
         if let encoded = try? JSONEncoder().encode(people) {
-            UserDefaults.standard.set(encoded, forKey: "SavedData")
+            UserDefaults.standard.set(encoded, forKey: saveKey)
         }
+    }
+
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
     }
 
     func toggle(_ prospect: Prospect) {
